fb03c40baaa804bb67a0e9024d21e1871a99bc7a
rtaladua <rtaladua@gmail.com>
2023-07-17 00:03:46 -0700

    Controllers

---
+++ controller/skills.js
@@ -0,0 +1,45 @@
+const SkillModel = require('../models/skill')
+
+module.exports = {
+	index: index,
+	show,
+	new: newSkill,
+	create,
+	delete: deleteOne
+	
+	// optionally
+	// index
+}
+
+function deleteOne(req, res){
+	SkillModel.deleteOne(req.params.id)
+	res.redirect('/skills')
+}
+
+
+
+
+function create(req, res){
+	console.log(req.body, " < _ contents of our form");
+	
+	SkillModel.create(req.body)
+	res.redirect('/skills');
+}
+
+
+function newSkill(req, res){
+
+
+    res.render('skills/new') 
+ }
+ 
+ function show(req, res){
+     console.log(req.params.id, " <- req.params.id")
+ 
+     res.render('skills/show', {skill: SkillModel.getOne(req.params.id) })
+ }
+ 
+ function index(req, res, next) {
+	
+	res.render('skills/index.ejs', {skills: SkillModel.getAll() })
+}
